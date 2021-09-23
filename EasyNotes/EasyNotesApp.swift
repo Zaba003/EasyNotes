4076d686bf0ea9bf997a5477975bb69c879bfb69
Kirill Zaborsky <avto.oil@gmail.com>
2021-09-23 20:44:56 +0700

    Initial Commit

---
+++ EasyNotes/EasyNotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  EasyNotesApp.swift
+//  EasyNotes
+//
+//  Created by Кирилл Заборский on 23.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct EasyNotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
